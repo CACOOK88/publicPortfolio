6a5fc7fa3a76b7d1d220d42f39513d40ee6361bb
Chad Cook <chadcookcoding@gmail.com>
2019-03-26 16:02:55 -0400

    Added projects to work page. Updated contact info. Changed some styling and made mobile responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,27 @@ const menu = document.querySelector('.menu')
 const menuNav = document.querySelector('.menu-nav')
 const menuBranding = document.querySelector('.menu-branding')
 const navItems = document.querySelectorAll('.nav-item')
+const juKwitYetImg = document.getElementById('juKwitYetImg')
+const onYourMarkImg = document.getElementById('onYourMarkImg')
 
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu)
+
+juKwitYetImg.addEventListener('mouseover', function() {
+  juKwitYetImg.src = '/assets/img/juKwitYetProfile.jpeg'
+})
+juKwitYetImg.addEventListener('mouseleave', function() {
+  juKwitYetImg.src = '/assets/img/juKwitYetHome.jpeg'
+})
+
+onYourMarkImg.addEventListener('mouseover', function() {
+  onYourMarkImg.src = '/assets/img/onYourMarkRaces.jpeg'
+})
+onYourMarkImg.addEventListener('mouseleave', function() {
+  onYourMarkImg.src = '/assets/img/onYourMark.jpeg'
+})
+
 
 function toggleMenu(){
   if (!showMenu) {
